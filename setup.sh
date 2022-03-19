628ab9630fcbd78403c37b0c5a9f528afeb61864
Jenny Belanger <jbelangr@gmail.com>
2022-03-19 19:41:44 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ file=".env"
 if [[ ! -f "${file}" ]]; then
 	cp "${file}.example" "${file}"
 	cp "${file}.example" "${file}.production"
-	docker exec -it --user ubuntu web sh -c 'sed --in-place "s/.local/.com/" "/var/www/fools/.env.production"'
+	docker exec -it --user ubuntu web sh -c 'sed --in-place "s/.local/.com/" "/var/www/corrie-admin/.env.production"'
 fi
 
 if [[ ! -f "cypress.env.json" ]]; then
